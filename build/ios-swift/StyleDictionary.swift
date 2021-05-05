30aab1ae67ef3185b8662941932eda89b78b6b9e
Automated Publisher <actions@users.noreply.github.com>
2021-05-05 09:55:10 +0000

    Automated publish: Wed May  5 09:55:10 UTC 2021 74fda53ca9a4790adfe15b63252752b20c614d13

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -3,7 +3,7 @@
 // StyleDictionary.swift
 //
 // Do not edit directly
-// Generated on Wed, 05 May 2021 09:53:21 GMT
+// Generated on Wed, 05 May 2021 09:55:10 GMT
 //
 
 
--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -3,7 +3,7 @@
 // StyleDictionarySize.swift
 //
 // Do not edit directly
-// Generated on Wed, 05 May 2021 09:53:21 GMT
+// Generated on Wed, 05 May 2021 09:55:10 GMT
 //
 
 import UIKit
